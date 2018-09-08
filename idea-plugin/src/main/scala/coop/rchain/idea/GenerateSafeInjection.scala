0b6e9751e6d6def66d0399f0a6272077567d94cc
Daniyar Itegulov <daniyar@pyrofex.net>
2018-09-08 15:48:05 +1000

    Support additional generated methods for safe classes

--- idea-plugin/src/main/scala/coop/rchain/idea/GenerateSafeInjection.scala
+++ idea-plugin/src/main/scala/coop/rchain/idea/GenerateSafeInjection.scala
@@ -22,27 +22,43 @@ class GenerateSafeInjection extends SyntheticMembersInjector {
         ScalaPsiUtil.getCompanionModule(obj) match {
           case Some(clazz: ScClass)
               if clazz.findAnnotationNoAliases("coop.rchain.models.macros.GenerateSafe") != null =>
-            val safeParams = clazz.parameters.map { parameter =>
+            val safeClassName = clazz.name + "Safe"
+            val safeParamsAndMethods = clazz.parameters.map { parameter =>
               parameter.`type`() match {
                 case Right(ScParameterizedType(des, Seq(tpe)))
                     if des.canonicalText == "_root_.scala.Option" =>
-                  convertToSafeTpe(tpe).map(parameter.name + ": " + _)
+                  convertToSafeTpe(tpe).map { safeTpe =>
+                    (
+                      parameter.name + ": " + safeTpe,
+                      s"def with${parameter.name.capitalize}(v: $safeTpe): $safeClassName = ???"
+                    )
+                  }
                 case Right(tpe) =>
-                  Some(parameter.name + ": " + tpe.canonicalText)
+                  Some(
+                    (
+                      parameter.name + ": " + tpe.canonicalText,
+                      s"def with${parameter.name.capitalize}(v: ${tpe.canonicalText}): $safeClassName = ???"
+                    )
+                  )
                 case Left(_) =>
                   None
               }
             }
-            if (safeParams.forall(_.isDefined)) {
-              val generatedParams = safeParams.map(_.get) :+ s"underlying: ${clazz.name}"
+            if (safeParamsAndMethods.forall(_.isDefined)) {
+              val (safeParams, withMethods) = safeParamsAndMethods.map(_.get).unzip
+              val generatedParams           = safeParams :+ s"underlying: ${clazz.name}"
               logger.info(s"Class ${clazz.name} has safe params: ${generatedParams.mkString(", ")}")
-              val safeClassName = clazz.name + "Safe"
               Seq(
-                s"final case class $safeClassName (${generatedParams.mkString(", ")})",
+                s"""final case class $safeClassName (${generatedParams.mkString(", ")}) {
+                   |  def toByteString: com.google.protobuf.ByteString = ???
+                   |  def toByteArray: Array[Byte] = ???
+                   |  ${withMethods.mkString("\n")}
+                   |}
+                 """.stripMargin,
                 s"""object $safeClassName {
                    |  def create(underlying: ${clazz.name}): Option[$safeClassName] = ???
                    |}
-               """.stripMargin
+                 """.stripMargin
               )
             } else {
               logger.warn(s"Couldn't derive some of parameters types for ${clazz.name}")
